041290ac9de6ed205a0bf19c4d602f54c80425e5
Simone Rava <ravasimoneit@gmail.com>
2021-11-18 17:01:40 +0100

    created the first version of the list of tasks

--- app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
+++ app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
@@ -1,18 +1,24 @@
 package com.example.androidapp.ui.tasks;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import android.widget.ArrayAdapter;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.androidapp.R;
 import com.example.androidapp.databinding.FragmentTasksBinding;
+
+import java.util.ArrayList;
 
 
 public class TasksFragment extends Fragment {
@@ -25,8 +31,16 @@ public class TasksFragment extends Fragment {
 
         binding = FragmentTasksBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        final TextView textView = binding.textNotifications;
-       textView.setText("this is Task fragment");
+       // final TextView textView = binding.textNotifications;
+       //textView.setText("this is Task fragment");'
+
+        //temp creation of data
+        ArrayList<String> listOfTasks =new ArrayList<>();
+        for(int i=0;i<20;i++)
+            listOfTasks.add("Task numero"+i);
+        final ListView myListView = (ListView) root.findViewById(R.id.ListView_tasks);
+        final ArrayAdapter <String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,listOfTasks);
+        myListView.setAdapter(adapter);
         return root;
     }
 

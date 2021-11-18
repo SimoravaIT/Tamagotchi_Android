ad1bb93a78c0bdd961bbe9b0e6885b4330ab4e30
Simone Rava <ravasimoneit@gmail.com>
2021-11-18 19:05:07 +0100

    database created, and it upload in the list of tasks the names

---
+++ app/src/main/java/com/example/androidapp/DatabaseHelper.java
@@ -0,0 +1,56 @@
+package com.example.androidapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "DB_tamagotchi";
+
+
+    public static final String TABLE_TASK_NAME = "tasks";
+    public static final String KEY_ID = "id";
+    public static final String KEY_NAME = "title";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_REWARD = "reward";
+    public static final String KEY_STEPS = "numsteps";
+    public static final String KEY_LOCATION = "location";
+
+    // Default SQL for creating a table in a database
+    public static final String CREATE_TABLE_SQL = "CREATE TABLE " + TABLE_TASK_NAME + " (" +
+            KEY_ID + " INTEGER PRIMARY KEY, " + KEY_NAME  + " TEXT NOT NULL, " + KEY_DESCRIPTION+ " TEXT , "
+            + KEY_REWARD + " INTEGER , " + KEY_STEPS + " INTEGER , "
+            + KEY_LOCATION + " TEXT );";
+
+
+    // The constructor
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_SQL);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public int NumberOfRows(Context c,String tableName){
+        DatabaseHelper databaseHelper = new DatabaseHelper(c);
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.longForQuery(database, "SELECT COUNT(*) FROM "+tableName, null);
+        return numRows;
+    }
+
+
+
+}
--- app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
+++ app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
@@ -1,11 +1,16 @@
 package com.example.androidapp.ui.tasks;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import android.widget.ArrayAdapter;
@@ -15,10 +20,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.androidapp.DatabaseHelper;
 import com.example.androidapp.R;
 import com.example.androidapp.databinding.FragmentTasksBinding;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 
 public class TasksFragment extends Fragment {
@@ -31,6 +39,8 @@ public class TasksFragment extends Fragment {
 
         binding = FragmentTasksBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        DatabaseHelper databaseHelper = new DatabaseHelper(root.getContext());
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
        // final TextView textView = binding.textNotifications;
        //textView.setText("this is Task fragment");'
 
@@ -41,6 +51,53 @@ public class TasksFragment extends Fragment {
         final ListView myListView = (ListView) root.findViewById(R.id.ListView_tasks);
         final ArrayAdapter <String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,listOfTasks);
         myListView.setAdapter(adapter);
+
+        View view = inflater.inflate(R.layout.fragment_tasks,
+                container, false);
+        Button button = (Button) root.findViewById(R.id.button_populate);
+
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                List<String> tasks_from_db = new LinkedList<String>();
+                // Get the readable database
+                String[] col={DatabaseHelper.KEY_NAME};
+                Cursor cursor = database.query(DatabaseHelper.TABLE_TASK_NAME,
+                        col, null, null, null,
+                        null, null );
+
+                // iterate over returned elements
+                cursor.moveToFirst();
+                for (int index=0; index < cursor.getCount(); index++){
+                    tasks_from_db.add(cursor.getString(0));
+                    cursor.moveToNext();
+                }
+                database.close();
+
+                Integer numTask = tasks_from_db.size();
+
+                button.setText(String.valueOf(numTask));
+
+                final ArrayAdapter <String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,tasks_from_db);
+                myListView.setAdapter(adapter);
+
+
+            }
+        });
+
+//add datas to the db by force it
+      /*  DatabaseHelper databaseHelper = new DatabaseHelper(root.getContext());
+        SQLiteDatabase database = databaseHelper.getReadableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.KEY_NAME,"nomee");
+        values.put(DatabaseHelper.KEY_DESCRIPTION, "descrizione");
+        values.put(DatabaseHelper.KEY_REWARD, 400);
+        values.put(DatabaseHelper.KEY_STEPS, 300);
+        values.put(DatabaseHelper.KEY_LOCATION, "afgan");
+        database.insert(DatabaseHelper.TABLE_TASK_NAME, null, values);
+*/
         return root;
     }
 

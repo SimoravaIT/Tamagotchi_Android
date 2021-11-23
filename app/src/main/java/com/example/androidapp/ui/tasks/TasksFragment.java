97e4093d6a803c702cd0f8f917a26c525c815a7f
Simone Rava <ravasimoneit@gmail.com>
2021-11-23 21:53:01 +0100

    add tasks in onCreate

--- app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
+++ app/src/main/java/com/example/androidapp/ui/tasks/TasksFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -21,11 +22,12 @@ import com.example.androidapp.Task;
 import com.example.androidapp.TaskController;
 import com.example.androidapp.databinding.FragmentTasksBinding;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 
-public class TasksFragment extends Fragment {
+public class TasksFragment extends Fragment implements AdapterView.OnItemClickListener{
 
 
     private FragmentTasksBinding binding;
@@ -40,11 +42,13 @@ public class TasksFragment extends Fragment {
 
         //listView definition
         final ListView myListView = (ListView) root.findViewById(R.id.ListView_tasks);
+        myListView.setOnItemClickListener(this);
 
         TaskController tc = new TaskController(root.getContext());
 
         List<Task> tasks = databaseHelper.loadAvailableTasks(root.getContext());
-
+        ArrayList<String> list_tasks_avaiable=tasks_names(tasks);
+        myListView.setAdapter(new ArrayAdapter<>(getActivity(), R.layout.task_layout, R.id.label,list_tasks_avaiable));
         //can be useful for View context
         View view = inflater.inflate(R.layout.fragment_tasks,
                 container, false);
@@ -53,9 +57,35 @@ public class TasksFragment extends Fragment {
         //TODO: Implement the AVAILABLE (!!) tasks visualization.
     }
 
+    private ArrayList tasks_names(List<Task> list) {
+        ArrayList<String> array_temp = new ArrayList<>();
+        for(Task t:list){
+            array_temp.add(t.getKey());
+        }
+        return array_temp;
+    }
+
+
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        //  ListView myListView = (ListView) getView().findViewById(R.id.ListView_tasks);
+        //Log.i("HelloListView", "You are here: "+myListView.getChildCount());
+        //LinearLayout ll = (LinearLayout) myListView.getChildAt(position);
+        //MaterialTextView text = (MaterialTextView) ll.getChildAt(0);
+        //Log.i("HelloListView", "my name is --->" + text.getText());
+        //Log.i("HelloListView", "my description is--->" + DatabaseHelper.
+        //      ObtainTaskDescription(getContext(), String.valueOf(text.getText())));
+        //String descr=DatabaseHelper.
+        //      ObtainTaskDescription(getContext(), String.valueOf(text.getText()));
+
+        //Toast.makeText(getActivity(),"DESCRIPTION: " + descr,Toast.LENGTH_LONG).show();
     }
 }

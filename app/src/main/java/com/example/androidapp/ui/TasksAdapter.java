3295d70275bc46ea9bf32a4438d99c4b9f5ceb03
Simone Rava <ravasimoneit@gmail.com>
2021-11-24 00:36:29 +0100

    adapter to manage task rows

---
+++ app/src/main/java/com/example/androidapp/ui/TasksAdapter.java
@@ -0,0 +1,41 @@
+package com.example.androidapp.ui;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.androidapp.R;
+import com.example.androidapp.Task;
+
+import java.util.ArrayList;
+
+public class TasksAdapter extends ArrayAdapter<Task> {
+    private final Context context;
+    private final ArrayList<Task> values;
+
+    public TasksAdapter(Context context, ArrayList<Task> values) {
+        super(context, R.layout.task_layout, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.task_layout, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.label);
+        TextView textView2 = (TextView) rowView.findViewById(R.id.collect);
+        textView.setText((CharSequence) values.get(position).getDescription());
+        // Change the icon for Windows and iPhone
+        textView2.setText("coins: "+String.valueOf(values.get(position).getReward()));
+
+
+        return rowView;
+    }
+}

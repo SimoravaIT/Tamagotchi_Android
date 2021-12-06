89eedccb7ba7449bd3faf145eca87e5e10bd7fde
edogab33 <edo.gab33@gmail.com>
2021-12-06 20:11:39 +0100

    Add completion of tasks logic

--- app/src/main/java/com/example/androidapp/TaskController.java
+++ app/src/main/java/com/example/androidapp/TaskController.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Random;
 import java.util.Set;
+import java.text.SimpleDateFormat;
 
 public class TaskController {
 
@@ -47,11 +48,43 @@ public class TaskController {
         }
     }
 
-    public void completeTask(Context context) {
-        // Check if the user completed the task correctly and delete it from AvailableTask.
+    public boolean completeTasks(Context context) {
+        // Check if the user completed any task correctly and delete it from AvailableTask.
+        List<Task> atasks = DatabaseController.loadAvailableTasks(context);
+
+        Date c = Calendar.getInstance().getTime();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String today = df.format(c);
+        int todaySteps = DatabaseController.loadStepsForTheDay(context, today);
+        boolean completed = false;
+
+        for (int i = 0; i < atasks.size(); i++) {
+            Log.d("TASKS", String.valueOf(atasks.get(i).getNumSteps()));
+            if (atasks.get(i).getNumSteps() > 0) {
+                // Task is about completing steps
+                if (taskIsCompleted(atasks.get(i).getNumSteps(), todaySteps)) {
+                    collectReward(context, atasks.get(i));
+                    DatabaseController.completeAvailableTask(context, atasks.get(i).getKey());
+                    completed = true;
+                }
+            } else {
+                // Task is about something else
+            }
+        }
+        return completed;
     }
 
-    public void collectReward(Context context, Task task) {
+    private boolean taskIsCompleted(int req, int state) {
+        // Input: 'req' is the number of steps/time outside required by the task
+        //        'state' is the actual state of steps/time outside performed by the user
+        if (state >= req) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void collectReward(Context context, Task task) {
         // Allocate money in the user's wallet according to the task.
         User user = DatabaseController.loadUser(context);
         user.setMoney(task.getReward());
@@ -66,7 +99,7 @@ public class TaskController {
         Random rand = new Random();
         Set<Integer> unique = new HashSet<Integer>();
         int upperbound = 3;
-        int toBeGenerated = 3;
+        int toBeGenerated = 2;
         int[] arr = new int[toBeGenerated];
         for (int i=0; i<arr.length; i++) {
             int number = rand.nextInt(upperbound);

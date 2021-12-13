5148de2e9de2f28c8cb64970c21c0103fa7bf50a
YuanjenHung <a0986859219@gmail.com>
2021-12-13 13:19:03 +0100

    Adjust the animation

--- app/src/main/java/com/example/androidapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/androidapp/ui/home/HomeFragment.java
@@ -31,17 +31,16 @@ public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private RelativeLayout chic_walking_area;
-    private GifImageView chic;
+    private GifImageView chic, heart;
     private LinearLayout food_group;
     private ImageView food_1, food_2, food_3, food_4, food_5;
-    private RelativeLayout.LayoutParams layoutParams;
+    private RelativeLayout.LayoutParams lp_bird, lp_heart;
     private int x_chic, y_chic;
     private int next_x_direction, next_y_direction;
     private int animation_frame, animation_frameSet_size;
+    private int eating_period;
     private String chic_state;
     private String food_selected;
-
-
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -58,13 +57,15 @@ public class HomeFragment extends Fragment {
         // layout, view, img init
         chic_walking_area = (RelativeLayout) root.findViewById(R.id.relative_screen_layout);
         chic = (GifImageView) root.findViewById(R.id.chic);
+        heart = (GifImageView) root.findViewById(R.id.heart);
         food_group = (LinearLayout) root.findViewById(R.id.food_group);
         food_1 = (ImageView) root.findViewById(R.id.food_selected_1);
         food_2 = (ImageView) root.findViewById(R.id.food_selected_2);
         food_3 = (ImageView) root.findViewById(R.id.food_selected_3);
         food_4 = (ImageView) root.findViewById(R.id.food_selected_4);
         food_5 = (ImageView) root.findViewById(R.id.food_selected_5);
-        layoutParams = new RelativeLayout.LayoutParams(160, 160);
+        lp_bird = new RelativeLayout.LayoutParams(160, 160);
+        lp_heart = new RelativeLayout.LayoutParams(120, 120);
 
         // start the animation
         startAnimation(chic_walking_area, chic);
@@ -78,10 +79,11 @@ public class HomeFragment extends Fragment {
         y_chic = 200;
         // animation frame init
         animation_frame = 0;
-        animation_frameSet_size = 10;
+        animation_frameSet_size = 15;
+        eating_period = 0;
         // chic state and food init
         chic_state = "walking_to_food";
-        food_selected = "branch";
+        food_selected = "worm";
 
 
         Timer mTimer = new Timer();
@@ -108,65 +110,105 @@ public class HomeFragment extends Fragment {
                             // change chic x, y based on the direction
                             x_chic += next_x_direction*5;
                             y_chic += next_y_direction*5;
-                            layoutParams.leftMargin = x_chic;
-                            layoutParams.topMargin = y_chic;
+                            lp_bird.leftMargin = x_chic;
+                            lp_bird.topMargin = y_chic;
 
                             // update the view
                             area.removeAllViews();
-                            area.addView(chic, layoutParams);
+                            area.addView(chic, lp_bird);
 
                             animation_frame++;
                             animation_frame%=animation_frameSet_size;
                         } else if (chic_state == "walking_to_food") {
                             if (food_group.getVisibility() == View.INVISIBLE) {
+                                if (food_selected == "branch") {
+                                    food_1.setImageResource(R.drawable.ic_branch);
+                                    food_2.setImageResource(R.drawable.ic_branch);
+                                    food_3.setImageResource(R.drawable.ic_branch);
+                                    food_4.setImageResource(R.drawable.ic_branch);
+                                    food_5.setImageResource(R.drawable.ic_branch);
+                                } else if (food_selected == "worm") {
+                                    food_1.setImageResource(R.drawable.ic_worm);
+                                    food_2.setImageResource(R.drawable.ic_worm);
+                                    food_3.setImageResource(R.drawable.ic_worm);
+                                    food_4.setImageResource(R.drawable.ic_worm);
+                                    food_5.setImageResource(R.drawable.ic_worm);
+                                } else if (food_selected == "lettuce") {
+                                    food_1.setImageResource(R.drawable.ic_lettuce);
+                                    food_2.setImageResource(R.drawable.ic_lettuce);
+                                    food_3.setImageResource(R.drawable.ic_lettuce);
+                                    food_4.setImageResource(R.drawable.ic_lettuce);
+                                    food_5.setImageResource(R.drawable.ic_lettuce);
+                                }
                                 food_group.setVisibility(View.VISIBLE);
                             }
-                            if (Math.abs(170 - x_chic) > 40 || Math.abs(95 - y_chic) > 10) {
+                            if (Math.abs(170 - x_chic) > 40 || Math.abs(85 - y_chic) > 10) {
                                 next_x_direction = 0;
                                 next_y_direction = 0;
                                 if (170 - x_chic > 40)  next_x_direction = 1;
                                 if (170 - x_chic < -40)  next_x_direction = -1;
-                                if (95 - y_chic > 10)  next_y_direction = 1;
-                                if (95 - y_chic < -10)  next_y_direction = -1;
+                                if (85 - y_chic > 10)  next_y_direction = 1;
+                                if (85 - y_chic < -10)  next_y_direction = -1;
                                 if (next_x_direction != 0) chic.setScaleX(-next_x_direction);
                                 x_chic += next_x_direction*5;
                                 y_chic += next_y_direction*5;
-                                layoutParams.leftMargin = x_chic;
-                                layoutParams.topMargin = y_chic;
+                                lp_bird.leftMargin = x_chic;
+                                lp_bird.topMargin = y_chic;
                                 area.removeAllViews();
-                                area.addView(chic, layoutParams);
+                                area.addView(chic, lp_bird);
                             } else {
                                 chic.setImageResource(R.drawable.chic_eating_animation);
-                                area.removeAllViews();
-                                area.addView(chic, layoutParams);
+//                                area.removeAllViews();
+//                                area.addView(chic, lp_bird);
                                 chic_state = "eating";
                             }
                         } else if (chic_state == "eating") {
+                            if (heart.getVisibility() == View.INVISIBLE) {
+                                heart.setVisibility(View.VISIBLE);
+                            }
                             if (animation_frame == 0) {
                                 next_x_direction = new Random().nextInt(3) + -1;
                                 if (x_chic > 300) next_x_direction = -1;
                                 if (x_chic < 30) next_x_direction = 1;
                                 if (next_x_direction != 0) chic.setScaleX(-next_x_direction);
                                 chic.setImageResource(R.drawable.chic_walking_animation);
-                                area.removeAllViews();
-                                area.addView(chic, layoutParams);
+//                                area.removeAllViews();
+//                                area.addView(heart, lp_bird);
+//                                area.addView(chic, lp_bird);
                             } else if (animation_frame < 3 && next_x_direction != 0) {
                                 x_chic += next_x_direction*5;
-                                layoutParams.leftMargin = x_chic;
+                                lp_bird.leftMargin = x_chic;
+                                lp_heart.leftMargin = x_chic + 110;
+                                lp_heart.topMargin = y_chic - 55;
                                 area.removeAllViews();
-                                area.addView(chic, layoutParams);
+                                area.addView(chic, lp_bird);
+                                area.addView(heart, lp_heart);
+
                             } else if (animation_frame == 3) {
                                 chic.setImageResource(R.drawable.chic_eating_animation);
-                                area.removeAllViews();
-                                area.addView(chic, layoutParams);
+//                                area.removeAllViews();
+//                                area.addView(heart, lp_bird);
+//                                area.addView(chic, lp_bird);
+                            } else if (animation_frame > 3) {
+                                heart.setVisibility(View.VISIBLE);
+                                food_group.setAlpha((float)(food_group.getAlpha()*0.99));
                             }
                             animation_frame++;
+                            eating_period++;
                             animation_frame%=animation_frameSet_size;
+                            if (eating_period > 200) {
+                                chic.setImageResource(R.drawable.chic_walking_animation);
+                                area.removeAllViews();
+                                area.addView(chic, lp_bird);
+                                food_group.setVisibility(View.INVISIBLE);
+                                heart.setVisibility(View.INVISIBLE);
+                                chic_state = "idle";
+                            }
                         }
                     });
                 }
             }
-        }, 0, 300);
+        }, 0, 100);
     }
 
     @Override
